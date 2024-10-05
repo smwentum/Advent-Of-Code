be89648cde10d3205d05e17f852630c032782092
smwentum <sai.wentum@hotmail.com>
2024-10-05 19:05:49 -0400

    did day 1 part 2

--- 2019/CPP Oct 2024/Day 1/Day 1.cpp
+++ 2019/CPP Oct 2024/Day 1/Day 1.cpp
@@ -16,22 +16,58 @@ int getValue(string val)
    return value;
 }
 
+
+int getValueP2(int value)
+{
+   value /= 3; 
+   value -=2;
+   return value;
+}
+
+int getValueP2(string val)
+{
+   int sum = 0;
+   int value =  std::stoi(val);
+
+   while(value > 0)
+   {
+     value = getValueP2(value); 
+   
+     if(value > 0)
+     {
+        //cout << value << endl;
+        sum += value;
+     }
+     else
+     {
+        break;
+     } 
+   }    
+   
+   return sum;
+}
+
 int main() 
 {
     string fileName = "final.txt"; 
     std::ifstream file(fileName);
     std::string str;
-    long long int sum = 0; 
-    int value {0};
+    long long int sumPart1 = 0; 
+    long long int sumPart2 = 0; 
+    
 
     while(std::getline(file,str))
     {
-        value = getValue(str);
+
         //cout  << str << ": " << value << endl;
-        sum += value;
+        sumPart1 +=  getValue(str);
+        //cout << str << " "<< getValueP2(str) << endl;
+        sumPart2 +=  getValueP2(str);
     }
 
-    cout << "Part 1: " << sum << endl;
+    cout << "Part 1: " << sumPart1 << endl;
+    cout << "Part 2: " << sumPart2 << endl;
+
 
 
 }
