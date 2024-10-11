8f096bc000e4cc37b504f2f8e758c650a66b2a2d
smwentum <sai.wentum@hotmail.com>
2024-10-11 19:18:31 -0400

    did day 2 part 2

--- 2015/Golang Oct 2024/Day 2/main.go
+++ 2015/Golang Oct 2024/Day 2/main.go
@@ -13,8 +13,8 @@ func main() {
 	path := "C:\\Users\\Sai Wentum\\Documents\\Programming Projects\\Advent Of Code\\2015\\Golang Oct 2024\\textFiles\\"
 	fileName := "day2Final.txt"
 	PartOne(path, fileName)
-	//fileName = "day1part2test.txt"
-	//PartTwo(path, fileName)
+	//fileName = "day2part1test.txt"
+	PartTwo(path, fileName)
 }
 
 func PartOne(path, fileName string) {
@@ -53,4 +53,32 @@ func getWrappingPaperDimensions(length, width, height int) int {
 	totalWrappingPaper := 2*(length*width+length*height+width*height) + min(length*width, min(length*height, width*height))
 
 	return totalWrappingPaper
+}
+
+func PartTwo(path, fileName string) {
+	//fmt.Println(path + fileName)
+	//fmt.Println()
+	file, err := os.Open(path + fileName)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	scanner := bufio.NewScanner(file)
+	sum := 0
+	for scanner.Scan() {
+		dimensions := strings.Split(scanner.Text(), "x")
+
+		sum += getWrappingRibbonDimensions(getNumberReturn0IfError(dimensions[0]),
+			getNumberReturn0IfError(dimensions[1]),
+			getNumberReturn0IfError(dimensions[2]))
+	}
+	fmt.Println("Answer to Day 1 Part 2:", sum)
+
+}
+
+func getWrappingRibbonDimensions(length, width, height int) int {
+	totalWrappingRibbon := length*width*height + 2*min(length+width, min(length+height, width+height))
+
+	return totalWrappingRibbon
 }
