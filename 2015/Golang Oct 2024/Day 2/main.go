2629bc63529d9353d69eb3abc26dedcb3f361c25
smwentum <sai.wentum@hotmail.com>
2024-10-11 19:11:01 -0400

    did day 2 part 1

---
+++ 2015/Golang Oct 2024/Day 2/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	path := "C:\\Users\\Sai Wentum\\Documents\\Programming Projects\\Advent Of Code\\2015\\Golang Oct 2024\\textFiles\\"
+	fileName := "day2Final.txt"
+	PartOne(path, fileName)
+	//fileName = "day1part2test.txt"
+	//PartTwo(path, fileName)
+}
+
+func PartOne(path, fileName string) {
+	//fmt.Println(path + fileName)
+	//fmt.Println()
+	file, err := os.Open(path + fileName)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	scanner := bufio.NewScanner(file)
+	sum := 0
+	for scanner.Scan() {
+		dimensions := strings.Split(scanner.Text(), "x")
+
+		sum += getWrappingPaperDimensions(getNumberReturn0IfError(dimensions[0]),
+			getNumberReturn0IfError(dimensions[1]),
+			getNumberReturn0IfError(dimensions[2]))
+	}
+	fmt.Println("Answer to Day 1 Part 1:", sum)
+
+}
+
+func getNumberReturn0IfError(s string) int {
+	n, err := strconv.Atoi(s)
+
+	if err != nil {
+		log.Fatal(err)
+		return 0
+	}
+	return n
+}
+
+func getWrappingPaperDimensions(length, width, height int) int {
+	totalWrappingPaper := 2*(length*width+length*height+width*height) + min(length*width, min(length*height, width*height))
+
+	return totalWrappingPaper
+}
