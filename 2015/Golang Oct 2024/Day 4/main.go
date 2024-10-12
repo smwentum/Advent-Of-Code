351cd5298933f6b5bc48f1c68730e3c239bb3850
smwentum <sai.wentum@hotmail.com>
2024-10-11 20:20:42 -0400

    did day 4 part 2

--- 2015/Golang Oct 2024/Day 4/main.go
+++ 2015/Golang Oct 2024/Day 4/main.go
@@ -15,7 +15,7 @@ func main() {
 	fileName := "day4final.txt"
 	partOne(path, fileName)
 	//fileName = "day3Test.txt"
-	//partTwo(path, fileName)
+	partTwo(path, fileName)
 }
 
 func partOne(path, fileName string) {
@@ -41,6 +41,37 @@ func findFirstSpecialMD5Hash(s string) int {
 		hash := GetMD5Hash(s + strconv.Itoa(i))
 		//fmt.Println("the value is i:", i, " :", hash[0:5])
 		if hash[0:5] == "00000" {
+			//fmt.Println("the value is i:", i, " :", hash[0:5])
+			return i
+		}
+		i += 1
+	}
+
+}
+
+func partTwo(path, fileName string) {
+	file, err := os.Open(path + fileName)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		fmt.Println(
+			"The answer to Day4Part2",
+			findFirstSpecialMD5HashPt2(scanner.Text()),
+		)
+	}
+}
+
+func findFirstSpecialMD5HashPt2(s string) int {
+	i := 1
+	for {
+		hash := GetMD5Hash(s + strconv.Itoa(i))
+		//fmt.Println("the value is i:", i, " :", hash[0:5])
+		if hash[0:6] == "000000" {
 			//fmt.Println("the value is i:", i, " :", hash[0:5])
 			return i
 		}
