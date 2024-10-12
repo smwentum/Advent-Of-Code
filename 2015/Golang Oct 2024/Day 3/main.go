dcf5b9a272b099e5eaaf7ff7c2fbbc32018e823f
smwentum <sai.wentum@hotmail.com>
2024-10-11 20:01:16 -0400

    did day 3 part 2

--- 2015/Golang Oct 2024/Day 3/main.go
+++ 2015/Golang Oct 2024/Day 3/main.go
@@ -12,7 +12,7 @@ func main() {
 	fileName := "day3final.txt"
 	partOne(path, fileName)
 	//fileName = "day3Test.txt"
-	//PartTwo(path, fileName)
+	partTwo(path, fileName)
 }
 
 func partOne(path, fileName string) {
@@ -55,6 +55,67 @@ func getNumberOfHouseWhoGotAPresent(directions string) int {
 
 	return len(houses)
 }
+
+
+
 func getStringFromTwoInts(row, col int) string {
 	return fmt.Sprintf("%d,%d", row, col)
+}
+
+func partTwo(path, fileName string) {
+
+	file, err := os.Open(path + fileName)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		fmt.Println("The answer to Day1Part1", getNumberOfHouseWhoGotAPresentPart2(scanner.Text()))
+	}
+
+}
+
+
+func getNumberOfHouseWhoGotAPresentPart2(directions string) int {
+
+	x := 0
+	y := 0
+	xRobo := 0
+	yRobo := 0
+
+	houses := make(map[string]int)
+	houses[getStringFromTwoInts(x, y)] += 2
+
+	for i, c := range directions {
+		if i%2 == 0 {
+			switch c {
+			case '^':
+				y += 1
+			case 'v':
+				y -= 1
+			case '>':
+				x += 1
+			case '<':
+				x -= 1
+			}
+			houses[getStringFromTwoInts(x, y)]++
+		} else {
+			switch c {
+			case '^':
+				yRobo += 1
+			case 'v':
+				yRobo -= 1
+			case '>':
+				xRobo += 1
+			case '<':
+				xRobo -= 1
+			}
+			houses[getStringFromTwoInts(xRobo, yRobo)]++
+		}
+	}
+
+	return len(houses)
 }
