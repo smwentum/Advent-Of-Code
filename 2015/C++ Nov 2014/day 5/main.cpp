f7ea56dd6a76d595afab3ce765604f43197f478b
smwentum <sai.wentum@hotmail.com>
2024-11-03 14:44:24 -0500

    did day 5 part1

--- 2015/C++ Nov 2014/day 5/main.cpp
+++ 2015/C++ Nov 2014/day 5/main.cpp
@@ -10,10 +10,15 @@ void day5partOne(std::string fileName);
 bool hasThreeVowels(std::string line);
 bool duplicateLetters(std::string line);
 bool doesNotContainString(std::string line);
+bool dupWithLetterInBetween(std::string line);
+void day5part2(std::string fileName);
+bool twoLetterDupeLaterInString(std::string line);
 
 int main()
 {
-    day5partOne("../test/day5partOne.txt");
+    std::string finalFileName = "../test/day5partOne.txt";
+    day5partOne(finalFileName);
+    day5part2(finalFileName);
 }
 
 void day5partOne(std::string fileName)
@@ -35,6 +40,27 @@ void day5partOne(std::string fileName)
 
     std::cout << "Answer to Day 5 part one: "<< count << "\n";
 }
+
+void day5part2(std::string fileName)
+{
+    std::string line{};
+
+    std::ifstream inFile(fileName);
+    int count{0};
+    while(std::getline(inFile,line))
+    {
+        bool dupeWithThingInMiddle = dupWithLetterInBetween(line);
+        bool twoLetterDupInString = twoLetterDupeLaterInString(line);
+        if(dupeWithThingInMiddle && twoLetterDupInString)
+        {
+                count++;
+        }
+
+    }
+
+    std::cout << "Answer to Day 5 part two: "<< count << "\n";
+}
+
 
 bool hasThreeVowels(std::string line)
 {
@@ -101,4 +127,30 @@ bool doesNotContainString(std::string line)
     return true;
 
 
+}
+
+bool dupWithLetterInBetween(std::string line)
+{
+    for(int i = 0; i < (int) line.size()-2;i++)
+    {
+        if(line[i] == line[i+2] && line[i] != line[i+1])
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+bool twoLetterDupeLaterInString(std::string line)
+{
+    for(int i = 0; i < (int) line.size()-3;i++)
+    {
+        std::string sub1 = line.substr(i,2);
+        //std::cout << line.substr(i+2)<< "\n";
+        if(line.substr(i+2).find(sub1)!= std::string::npos)
+        {
+            return true;
+        }
+    }
+    return false;
 }
