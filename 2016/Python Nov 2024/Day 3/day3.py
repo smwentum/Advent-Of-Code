8713d15043953871226d7c9e22116418cc7ef78d
smwentum <sai.wentum@hotmail.com>
2024-11-07 19:05:01 -0500

    did day 3 part 2

--- 2016/Python Nov 2024/Day 3/day3.py
+++ 2016/Python Nov 2024/Day 3/day3.py
@@ -1,6 +1,9 @@
 def main():
     fileName = "../TextFiles/day3.txt"
     partOne(fileName)
+    partTwo(fileName)
+
+
 
 def partOne(fileName):
     count = 0
@@ -23,5 +26,46 @@ def isValidTriangle(a,b,c):
     elif c + a <= b:
         return False
     return True
+
+
+def partTwo(fileName):
+    count = 0
+    with open(fileName,"r") as f:
+        l1 = []
+        l2 = []
+        l3 = []
+
+        for s in f.readlines():
+            #print(s)
+            (a ,b ,c) = s.split()
+            l1.append(int(a))
+            l2.append(int(b))
+            l3.append(int(c))
+        #l1.sort()
+        #l2.sort()
+        #l3.sort()
+       # print(l1)
+
+
+
+
+        for i in range(0,len(l1),3):
+            #print(i)
+            #print(l1[i],l1[i+1],l1[i+2])
+            if isValidTriangle(l1[i],l1[i+1],l1[i+2]):
+                count+= 1
+        for i in range(0,len(l2),3):
+            #print(l1[i],l1[i+1],l1[i+2])
+            if isValidTriangle(l2[i],l2[i+1],l2[i+2]):
+                count+= 1
+
+        for i in range(0,len(l3),3):
+            #print(l1[i],l1[i+1],l1[i+2])
+            if isValidTriangle(l3[i],l3[i+1],l3[i+2]):
+                count+= 1
+
+
+    print("Day 3 part 2:",count)
+
 
 main()
