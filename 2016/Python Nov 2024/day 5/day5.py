1a21785bcc388e3aa9322d1479981d3cdf7cffcc
smwentum <sai.wentum@hotmail.com>
2024-11-10 11:05:30 -0500

    did day 5 part 2

--- 2016/Python Nov 2024/day 5/day5.py
+++ 2016/Python Nov 2024/day 5/day5.py
@@ -2,7 +2,12 @@ import hashlib
 
 
 def main():
-    partOne("abbhdwsy")
+   
+    with open("./TestFiles/day5.txt") as f:
+        key = f.readline()
+    print(key)
+    partOne(key)
+    partTwo(key)
 
 def partOne(baseString): 
     s = 't'
@@ -15,6 +20,21 @@ def partOne(baseString):
         i+=1
         
     print(ans)
+
+def partTwo(baseString): 
+    s = ''
+    i = 1
+    ans = ['*'] *8
+    while '*' in ans :
+        s =  (hashlib.md5((baseString+str(i)).encode('utf-8')) ).hexdigest()
+        if s.startswith('00000'):
+            if str.isdigit( s[5]) and int(s[5]) < 8 and ans[int(s[5])] == '*'  :
+                #print(s)
+                ans[int(s[5])] =s[6]
+                #print(ans)
+        i+=1
+        
+    print(''.join(ans))
     
 
 main()
