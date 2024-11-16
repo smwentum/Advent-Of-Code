09a756f19e1af820f9a26d38bf6f6b32241c4494
smwentum <sai.wentum@hotmail.com>
2024-11-15 20:26:52 -0500

    day 1 complete

---
+++ 2020/c++ nov 2020/day 1/day1.cpp
@@ -0,0 +1,70 @@
+#include<iostream>
+#include<string>
+#include<fstream>
+#include<set>
+#include<vector>
+
+void dayOnePartOne(std::string fileName);
+void dayOnePartTwo(std::string fileName);
+
+int main()
+{
+
+    dayOnePartOne("../textfiles/day1final.txt");
+    dayOnePartTwo("../textfiles/day1final.txt");
+
+
+}
+
+
+void dayOnePartOne(std::string fileName)
+{
+    std::ifstream file(fileName);
+
+    std::string str;
+
+    std::set<int> lst;
+
+    while(std::getline(file,str))
+    {
+        int x =  std::stoi(str);
+        if( lst.count(2020 - x) == 1)
+        {
+            std::cout << "Day 1 part 1 answer: " << (2020 -x)*x << "\n";
+            break;
+        }
+        else
+        {
+            lst.insert(x);
+        }
+    }
+}
+
+
+void dayOnePartTwo(std::string fileName)
+{
+    std::ifstream file(fileName);
+
+    std::string str;
+
+    std::vector<int> vect;
+
+    while(std::getline(file,str))
+    {
+        vect.push_back(std::stoi(str));
+    }
+
+    for(int i = 0; i < (int) vect.size();i++)
+    {
+        for(int j = i+1; j < (int) vect.size();j++)
+        {
+            for(int k = j+1; k < (int)vect.size(); k++)
+            {
+                if(vect[i] +vect[j]+ vect[k] == 2020)
+                {
+                    std::cout <<"Day 1 Part 2: " << vect[i]*vect[j]*vect[k] << "\n";
+                }
+            }
+        }
+    }
+}
