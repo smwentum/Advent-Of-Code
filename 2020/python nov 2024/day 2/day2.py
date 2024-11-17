475f087e31a8fdfb20d654ca6990e3a347573960
smwentum <sai.wentum@hotmail.com>
2024-11-16 19:26:25 -0500

    did day 2 part one

---
+++ 2020/python nov 2024/day 2/day2.py
@@ -0,0 +1,33 @@
+
+def main():
+    part_one("../textfile/day2final.txt")
+    part_two("../textfile/day2final.txt")
+
+def part_one(file_name:str):
+    with open(file_name, encoding="utf-8") as f:
+        cnt = 0
+        for line in f.readlines():
+            split = line.split(" ")
+            low,high = [int(x) for x in  split[0].split("-")]
+            count_of_letters = split[2].count(split[1][0])
+            if count_of_letters >= low and count_of_letters <= high:
+                cnt+=1
+        print("Day two part one:",cnt)
+
+def part_two(file_name:str):
+    with open(file_name, encoding="utf-8") as f:
+        cnt = 0
+        for line in f.readlines():
+            split = line.split(" ")
+            low,high = [int(x) for x in  split[0].split("-")]
+            c = split[1][0]
+
+            if (split[2][low-1] == c and split[2][high-1] != c) or  (split[2][low-1] != c and split[2][high-1] == c):
+                cnt+=1
+            # count_of_letters = split[2].count(split[1][0])
+            # if count_of_letters >= low and count_of_letters <= high:
+            #     cnt+=1
+        print("Day 2 part two:",cnt)
+
+
+main()
