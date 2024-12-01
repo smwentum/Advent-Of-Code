bb6a697400f86fd3337368aa1239862910476e8b
smwentum <sai.wentum@hotmail.com>
2024-12-01 06:19:14 -0500

    did day 1 part 2

--- 2024/Dec 2024 Python/day 1/day1.py
+++ 2024/Dec 2024 Python/day 1/day1.py
@@ -1,9 +1,11 @@
 import heapq
+from collections import Counter
 
 
 def main():
     fileName = "../textfiles/day1.txt"
     part_one(fileName=fileName)
+    part_two(fileName=fileName)
 
 
 def part_one(fileName):
@@ -17,7 +19,7 @@ def part_one(fileName):
                 b = int(line.split()[1])
                 l1.append(a)
                 l2.append(b)
-  
+
         # i am pretty sure heapify is a min heap
         heapq.heapify(l1)
         heapq.heapify(l2)
@@ -27,6 +29,25 @@ def part_one(fileName):
             s += abs(heapq.heappop(l1) - heapq.heappop(l2))
 
         print("Day One Part One answer:", s)
+
+
+def part_two(fileName):
+    l1 = []
+    l2 = []
+    with open(fileName, encoding="utf8") as f:
+        lines = f.read().splitlines()
+        for line in lines:
+            if len(line) > 3:
+                a = int(line.split()[0])
+                b = int(line.split()[1])
+                l1.append(a)
+                l2.append(b)
+        count = Counter(l2)
+        ans = 0
+        for l in l1:
+            if l in count:
+                ans += l*count[l]
+        print("Day One Part Two answer:", ans)
 
 
 main()
