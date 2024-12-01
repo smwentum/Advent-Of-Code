849b352b58f1e7dc9bd71cd462fb624567ff4a52
smwentum <sai.wentum@hotmail.com>
2024-12-01 07:18:39 -0500

    did day one

--- 2024/Dec 2024 C++/Day 1/main.cpp
+++ 2024/Dec 2024 C++/Day 1/main.cpp
@@ -1,15 +1,19 @@
 #include <algorithm>
 #include <fstream>
 #include <iostream> 
+#include <math.h>
 #include <string>
 #include <vector>
-#include <math.h>
+#include <unordered_map>
+
 
 void partOne(std::string fileName);
+void partTwo(std::string fileName);
 
 int main()
 {
     partOne("../textfiles/day1.txt");
+    partTwo("../textfiles/day1.txt");
 }
 
 void partOne(std::string fileName)
@@ -39,28 +43,46 @@ void partOne(std::string fileName)
 
 
 
-// void partOne(std::string fileName)
-// {
+void partTwo(std::string fileName)
+{
 
-//     std::ifstream infile(fileName);
-//     std::vector<int> l1; 
-//     std::vector<int> l2;
-//     //read in the file
-//     long a, b;
-//     while (infile >> a >> b)
-//     {
-//         l1.push_back(a);
-//         l2.push_back(b);
-//     }
-//     std::sort(l1.begin(),l1.end());
-//     std::sort(l2.begin(),l2.end());
+    std::ifstream infile(fileName);
+    std::vector<int> l1; 
+    std::vector<int> l2;
+    //read in the file
+    long a, b;
+    while (infile >> a >> b)
+    {
+        l1.push_back(a);
+        l2.push_back(b);
+    }
+    std::sort(l1.begin(),l1.end());
+    std::sort(l2.begin(),l2.end());
     
-//     long sum = 0;
-//     for(int i =0 ; i < (int)l1.size();i++)
-//     {
-//         sum += abs(l1[i]-l2[i]);
-//     }
-//     std::cout << "Day One part one: " << sum << "\n";
+    std::unordered_map<long,int> map; 
 
-// }
+    long sum = 0;
+    for(int i =0 ; i < (int)l2.size();i++)
+    {
+        if(map.find(l2[i]) != map.end())
+        {
+            map[l2[i]]+=1;
+        }
+        else
+        {
+            map[l2[i]] = 1;
+        }
+    }
+    
+    for(int i =0 ; i < (int)l2.size();i++)
+    {
+        if(map.find(l1[i]) != map.end())
+        {
+            sum += map[l1[i]] *l1[i]; 
+        }
+
+    }
+    std::cout << "Day One part two: " << sum << "\n";
+
+}
 
