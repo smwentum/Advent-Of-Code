eb3db71e1b34e277f16e0dec35d6ed9ec3dde394
smwentum <sai.wentum@hotmail.com>
2024-12-03 21:35:31 -0500

    did day 2 part2

--- 2024/Dec 2024 Python/Day 3/day3.py
+++ 2024/Dec 2024 Python/Day 3/day3.py
@@ -1,10 +1,10 @@
 import re
-
+import heapq
 
 def main():
     fileName = "../textfiles/day3final.txt"
     part_one(fileName)
-    fileName = "../textfiles/day3test.txt"
+    fileName = "../textfiles/day3final.txt"
     part_two(fileName)
 
 
@@ -32,29 +32,69 @@ def part_two(fileName):
     with open(fileName, encoding="utf8") as f:
         for x in f.read().splitlines():
             line += x
-        # print(line)
+        #print(len(line))
         x = re.finditer(
             "mul\\([\\d]{1,3},[\\d]{1,3}\\)", line)
 
         find_do = re.finditer(
             "do\\(\\)", line)
+
+        heapDo = []
+
+        heapq.heapify(heapDo)
+        heapq.heappush(heapDo,len(line))
         for x_do in find_do:
-            print(x_do.start(), x_do.end(), x_do.group())
+            #print(x_do.start(), x_do.end(), x_do.group())
+            heapq.heappush(heapDo,x_do.start())
 
         find_dont = re.finditer(
             "don't\\(\\)", line)
 
-        for x_dont in find_dont:
-            print(x_dont.start(), x_dont.end(), x_dont.group())
+        heapDont = []
 
-        # sum_product = 0
-        # for m in x:
-        #     # print(m.group())
-        #     prod = m.group()[4:-1]
-        #     # print(prod)
-        #     a, b = prod.split(",")
-        #     sum_product += int(a)*int(b)
-        # print("Day one part one:", sum_product)
+        heapq.heapify(heapDont)
+
+        for x_dont in find_dont:
+            #print(x_dont.start(), x_dont.end(), x_dont.group())
+            heapq.heappush(heapDont,x_dont.start())
+
+        badIntervals = []
+        #print(heapDont)
+
+        # print("start", start)
+
+        while len(heapDo) > 0 and len(heapDont) > 0:
+            start =  heapq.heappop(heapDont)
+
+
+            while len(heapDo) > 0 and  heapDo[0] < start:
+                heapq.heappop(heapDo)
+
+            if len(heapDo) > 0 and  heapDo[0] > start:
+                badIntervals.append((start,heapDo[0]))
+
+
+
+
+
+
+
+
+        print("Bad intervals: ",badIntervals)
+        sum_product = 0
+        for m in x:
+            if  any( max(badIntervals[i][0],m.start()) < min(badIntervals[i][1],m.end())
+                     for i in range(len(badIntervals))):
+
+                continue
+
+            print(m.group(),m.start(),m.end())
+            prod = m.group()[4:-1]
+            # print(prod)
+            a, b = prod.split(",")
+            sum_product += int(a)*int(b)
+        print("Day one part two:", sum_product)
+
 
 
 main()
