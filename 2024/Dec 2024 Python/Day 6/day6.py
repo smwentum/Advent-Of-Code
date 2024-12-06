289a8d29fbc8bfea9cb9ef4a2fb9dd303847b6b0
smwentum <sai.wentum@hotmail.com>
2024-12-06 06:23:56 -0500

    did day 6 part 2 (kind of slow)

--- 2024/Dec 2024 Python/Day 6/day6.py
+++ 2024/Dec 2024 Python/Day 6/day6.py
@@ -1,7 +1,9 @@
 def main():
     fileName = "../textfiles/day6final.txt"
     part_one(file_name=fileName)
-    
+
+    fileName = "../textfiles/day6final.txt"
+    part_two(file_name=fileName)
 
 
 def part_one(file_name):
@@ -33,6 +35,64 @@ def part_one(file_name):
                 break
 
         print("Day 6 part One:", len(visitedPoints))
+
+
+def part_two(file_name):
+    with open(file=file_name, encoding="utf8") as f:
+        grid = f.read().splitlines()
+        # find the staring position
+        startX = 0
+        startY = 0
+        startDirection = (-1, 0)
+        cnt = 0
+        for i, _ in enumerate(grid):
+            for j, _ in enumerate(grid):
+                if grid[i][j] == '^':
+                    startX = i
+                    startY = j
+
+        for i, _ in enumerate(grid):
+            for j, _ in enumerate(grid):
+                if grid[i][j] == '.':
+                    grid[i] = stringReplace(grid[i], j, '#')
+                    if simulateGettingStuckInTheLoop(grid, startX, startY, startDirection):
+                        cnt += 1
+                    grid[i] = stringReplace(grid[i], j, '.')
+
+        print("Day 6 part two:", cnt)
+
+
+def stringReplace(s, index, value):
+    s1 = ""
+    for i, c in enumerate(s):
+        if i == index:
+            s1 += value
+        else:
+            s1 += c
+    return s1
+
+
+def simulateGettingStuckInTheLoop(grid, startX, startY, startDirection):
+
+    visitedPoints = set()
+
+    visitedPoints.add((startX, startY, startDirection))
+
+    while True:
+        # i need to figure out where to go next
+        if isValidInGrid(startX+startDirection[0], startY+startDirection[1], len(grid), len(grid[0])):
+            if grid[startX+startDirection[0]][startY+startDirection[1]] != "#":
+                startX = startX+startDirection[0]
+                startY = startY+startDirection[1]
+                if (startX, startY, startDirection) not in visitedPoints:
+                    visitedPoints.add((startX, startY, startDirection))
+                else:
+                    return True
+            else:
+                startDirection = changeDirection(startDirection)
+            # print(startX, startY)
+        else:
+            return False
 
 
 def isValidInGrid(x, y, maxRow, maxCol):
