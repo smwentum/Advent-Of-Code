b28d10e82d8cba342d5c7ee7ecb2f25003fbf5b0
smwentum <sai.wentum@hotmail.com>
2024-12-09 06:24:32 -0500

    did day8 part two

--- 2024/Dec 2024 Python/Day 8/day8.py
+++ 2024/Dec 2024 Python/Day 8/day8.py
@@ -2,8 +2,8 @@ def main():
 
     file_name = "../textfiles/day8final.txt"
     part_one(file_name)
-    # file_name = "../textfiles/day7final.txt"
-    # part_two(file_name)
+    file_name = "../textfiles/day8final.txt"
+    part_two(file_name)
 
 
 def part_one(file_name):
@@ -57,6 +57,82 @@ def part_one(file_name):
                         setOfNodes.add(pt4)
 
         print("Day Eight Part One:", len(setOfNodes))
+
+
+def part_two(file_name):
+    # get the lines
+
+    with open(file_name, encoding="utf8") as f:
+        cnt = 0
+        lines = f.read().splitlines()
+        rows = len(lines)
+        cols = len(lines[0])
+        d = dict()
+        nonemptySpot = []
+        for i, _ in enumerate(lines):
+            for j, _ in enumerate(lines[i]):
+                # make a dict of chars (not the dots) and put each point as tuple
+                if lines[i][j] != '.':
+                    if lines[i][j] not in d:
+                        d[lines[i][j]] = set()
+                    nonemptySpot.append((i, j))
+                    d[lines[i][j]].add((i, j))
+
+        # now that i have the points add the antinotes
+        setOfNodes = set()
+        lst1 = []
+        for group in d:
+            lst = list(d[group])
+            for i in range(len(lst)):
+                for j in range(i+1, len(lst)):
+                    # print(i)
+                    pt1 = lst[i]
+                    pt2 = lst[j]
+                    # print(pt1)
+                    # print(pt2)
+
+                    xDist = pt1[0]-pt2[0]
+                    yDist = pt1[1]-pt2[1]
+
+                    pt3 = ((pt1[0]-xDist), (pt1[1]-yDist))
+                    if pt3 == pt2:
+                        pt3 = ((pt1[0]+xDist), (pt1[1]+yDist))
+                        i1 = 2
+                        while isValidPoint(pt3, rows, cols):
+                            lst1.append(pt3)
+                            pt3 = ((pt1[0]+i1*xDist), (pt1[1]+i1*yDist))
+                            # print(pt3)
+                            i1 += 1
+
+                    else:
+                        i1 = 2
+                        while isValidPoint(pt3, rows, cols):
+                            lst1.append(pt3)
+                            pt3 = ((pt1[0]-i1*xDist), (pt1[1]-i1*yDist))
+                            i1 += 1
+
+                    pt4 = ((pt2[0]+xDist), (pt2[1]+yDist))
+                    if pt4 == pt1:
+                        pt4 = ((pt2[0]-xDist), (pt2[1]-yDist))
+                        i1 = 2
+                        while isValidPoint(pt4, rows, cols):
+                            lst1.append(pt4)
+                            pt4 = ((pt2[0]-i1*xDist), (pt2[1]-i1*yDist))
+                            i1 += 1
+                    else:
+                        i1 = 2
+                        while isValidPoint(pt4, rows, cols):
+                            lst1.append(pt4)
+                            pt4 = ((pt2[0]+i1*xDist), (pt2[1]+i1*yDist))
+                            i1 += 1
+
+            for l1 in lst1:
+                if isValidPoint(l1, rows, cols) and l1 not in setOfNodes:
+                    setOfNodes.add(l1)
+        # print(lst1)
+       # print("Day Eight Part Two:", len(setOfNodes))
+        # print("Day Eight Part Two:", len(setOfNodes)+nonemptySpot)
+        print("Day Eight Part Two:", len(setOfNodes.union(nonemptySpot)))
 
 
 def isValidPoint(pt, rows, cols):
