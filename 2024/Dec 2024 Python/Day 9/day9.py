228788f1f3aa5279fcb3d6f96b2db79109580b5e
smwentum <sai.wentum@hotmail.com>
2024-12-09 19:31:20 -0500

    had to look up the answer still not sure what i was doing wrong

--- 2024/Dec 2024 Python/Day 9/day9.py
+++ 2024/Dec 2024 Python/Day 9/day9.py
@@ -10,35 +10,46 @@ def part_one(filename):
         # print(line)
         decompated = []
         index = 0
-        isFreeSpace = False
-        for i in range(len(line)):
+
+        for i,_ in enumerate(line):
             number_of_times = int(line[i])
             # print(number_of_times)
-            if not isFreeSpace:
-                for _ in range(number_of_times):
-                    decompated.append(index)
+            if i%2 == 0:
+                decompated+=[index]*number_of_times
                 index += 1
             else:
-                for _ in range(number_of_times):
-                    decompated.append(-1)
-            isFreeSpace = not isFreeSpace
-        # print(decompated)
-        left = 0
-        right = len(decompated)-1
-        while left < right:
-            # print(left, right)
-            while left < len(decompated) and decompated[left] != -1:
-                left += 1
-            while right > -1 and decompated[right] == -1:
-                right -= 1
-            if (left < len(decompated) and decompated[left] == -1 and right > -1 and decompated[right] != -1):
-                decompated[left] = decompated[right]
-                decompated[right] = -1
-                left += 1
-                right -= 1
-        # print(decompated)
+                decompated+=[-1]*number_of_times
+
+        print(decompated)
+        blanks = [i for i,x in enumerate(decompated) if x == -1]
+
+        print("blanks",blanks)
+        for i in blanks:
+            #remove the blanks at the end
+            while decompated[-1] == -1:
+                decompated.pop()
+            if len(decompated) <= i:
+                    break
+            decompated[i]=decompated.pop()
+        print(decompated)
+        # left = 0
+        # right = len(decompated)-1
+        # while left < right:
+        #     # print(left, right)
+        #     while left < len(decompated) and decompated[left] != -1:
+        #         left += 1
+        #     while right > -1 and decompated[right] == -1:
+        #         right -= 1
+
+        #     if (left < len(decompated) and decompated[left] == -1 and right > -1 and decompated[right] != -1):
+        #         decompated[left] = decompated[right]
+        #         decompated[right] = -1
+        #         left += 1
+        #         right -= 1
+        # # print(decompated)
         print("Day 9 part One:", getCheckSum(decompated))
         # 6334393041104 -too low
+        # 6334655984939
 
 
 def getCheckSum(decompated):
@@ -47,9 +58,6 @@ def getCheckSum(decompated):
         # print(val, i)
         if val != -1:
             s += i*val
-
-        else:
-            break
     return s
 
 
