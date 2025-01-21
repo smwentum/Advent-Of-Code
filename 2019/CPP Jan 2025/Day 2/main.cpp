12b84305921f4397cfd4053299fe8a9181ec723f
smwentum <sai.wentum@hotmail.com>
2025-01-20 19:59:36 -0500

    did part 2

--- 2019/CPP Jan 2025/Day 2/main.cpp
+++ 2019/CPP Jan 2025/Day 2/main.cpp
@@ -4,15 +4,15 @@
 #include <vector>
 
 void partOne(std::string fileName);
+void partTwo(std::string fileName);
 
 int main(void)
 {
     std::string fileName = "../TestFiles/day2final.txt";
+
     partOne(fileName);
-
-
+    partTwo(fileName);
 }
-
 
 void partOne(std::string fileName)
 {
@@ -21,43 +21,100 @@ void partOne(std::string fileName)
     long sum{0};
     std::vector<int> codes;
 
-    //std::cout << "in part one";
+    // std::cout << "in part one";
     if (file.is_open())
     {
-        //std::cout << "File open";
-        while (std::getline(file, line,','))
+        // std::cout << "File open";
+        while (std::getline(file, line, ','))
         {
 
             codes.push_back(std::stoi(line));
-            //std::cout << std::endl << i << ". " << line << std::endl;
-
+            // std::cout << std::endl << i << ". " << line << std::endl;
         }
 
         codes[1] = 12;
         codes[2] = 2;
 
-        for(size_t i = 0; i  < codes.size(); i+=4)
+        for (size_t i = 0; i < codes.size(); i += 4)
         {
-            int v1 = codes[codes[i+1]];
-            int v2 = codes[codes[i+2]];
+            int v1 = codes[codes[i + 1]];
+            int v2 = codes[codes[i + 2]];
 
-            if(codes[i] == 99)
+            if (codes[i] == 99)
             {
                 break;
             }
 
-            switch(codes[i])
+            switch (codes[i])
             {
-                case 1:
+            case 1:
 
-                    codes[codes[i+3]] = v1 + v2;
-                    break;
-                case 2:
-                    codes[codes[i+3]] = v1 * v2;
-                    break;
+                codes[codes[i + 3]] = v1 + v2;
+                break;
+            case 2:
+                codes[codes[i + 3]] = v1 * v2;
+                break;
             }
-
         }
         std::cout << "Day two part one: " << codes[0] << std::endl;
+    }
+}
+
+void partTwo(std::string fileName)
+{
+    std::ifstream file(fileName);
+    std::string line;
+    long sum{0};
+    std::vector<int> codes1;
+    std::vector<int> codes;
+
+    // std::cout << "in part one";
+    if (file.is_open())
+    {
+        // std::cout << "File open";
+        while (std::getline(file, line, ','))
+        {
+
+            codes1.push_back(std::stoi(line));
+            // std::cout << std::endl << i << ". " << line << std::endl;
+        }
+
+
+        for (int noun = 0; noun < 100; noun++)
+        {
+            for (int verb = 0; verb < 100; verb++)
+            {
+                codes = codes1;
+                codes[1] = noun;
+                codes[2] = verb;
+
+                for (size_t i = 0; i < codes.size(); i += 4)
+                {
+                    int v1 = codes[codes[i + 1]];
+                    int v2 = codes[codes[i + 2]];
+
+                    if (codes[i] == 99)
+                    {
+                        break;
+                    }
+
+                    switch (codes[i])
+                    {
+                        case 1:
+
+                            codes[codes[i + 3]] = v1 + v2;
+                            break;
+                        case 2:
+                            codes[codes[i + 3]] = v1 * v2;
+                            break;
+                    }
+                }
+
+                if(codes[0] == 19690720)
+                {
+                    std::cout << "Day two part two: " << 100*noun+verb << std::endl;
+                }
+            }
+        }
     }
 }
