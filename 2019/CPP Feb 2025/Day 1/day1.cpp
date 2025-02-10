9e979d832be1eb73aa22ff22546951378d323311
smwentum <sai.wentum@hotmail.com>
2025-02-09 19:43:28 -0500

    finished day 1 the recursion keeps screwing me up

--- 2019/CPP Feb 2025/Day 1/day1.cpp
+++ 2019/CPP Feb 2025/Day 1/day1.cpp
@@ -5,11 +5,12 @@
 void partOne(std::string fileName);
 void partTwo(std::string fileName);
 int getNewValue(int oldValue);
+int getNewValuePt2(int oldValue);
 
 int main()
 {
     partOne("../textfiles/day1final.txt");
-
+    partTwo("../textfiles/day1final.txt");
     return 0;
 }
 
@@ -22,7 +23,7 @@ void partOne(std::string fileName)
 
     while (textFile >> num)
     {
-        std::cout << num << std::endl;
+        // std::cout << num << std::endl;
         vals.push_back(num);
     }
 
@@ -40,4 +41,42 @@ int getNewValue(int oldValue)
     oldValue /= 3;
     oldValue -= 2;
     return oldValue;
+}
+
+void partTwo(std::string fileName)
+{
+    // read the lines
+    std::ifstream textFile(fileName);
+    int num;
+    std::vector<int> vals;
+
+    while (textFile >> num)
+    {
+        // std::cout << num << std::endl;
+        vals.push_back(num);
+    }
+
+    int s{0};
+    // std::cout << getNewValuePt2(1969) << std::endl;
+    for (int v : vals)
+    {
+        s += getNewValuePt2(v);
+    }
+
+    std::cout << "Day 1 part 2: " << s << std::endl;
+}
+
+int getNewValuePt2(int oldValue)
+{
+    if (oldValue < 1)
+    {
+        return 0;
+    }
+    int v = getNewValue(oldValue);
+    // std::cout << v << std::endl;
+    if (v <= 0)
+    {
+        return 0;
+    }
+    return v + getNewValuePt2(v);
 }
