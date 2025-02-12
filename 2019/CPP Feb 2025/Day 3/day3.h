c5b4b4c669227257381476fdd092e477013f37d5
smwentum <sai.wentum@hotmail.com>
2025-02-12 06:58:50 -0500

    almost got part 2

--- 2019/CPP Feb 2025/Day 3/day3.h
+++ 2019/CPP Feb 2025/Day 3/day3.h
@@ -8,5 +8,7 @@ void partOne(std::string fileName);
 std::vector<std::vector<std::string>> getDirections(std::string fileName);
 
 std::set<std::tuple<int, int>> getAllPoints(std::vector<std::string> directions);
+void partTwo(std::string fileName);
+std::map<std::tuple<int, int>, int> getAllPointsPt2(std::vector<std::string> directions);
 
 #endif
