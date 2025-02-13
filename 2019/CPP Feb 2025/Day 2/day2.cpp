ff050ce68c9409cda82174917a0ccd46ab6926fb
smwentum <sai.wentum@hotmail.com>
2025-02-12 19:31:27 -0500

    didn't read the directions :-(

---
+++ 2019/CPP Feb 2025/Day 2/day2.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+
+#include "day2.h"
+
+int main()
+{
+    std::string fileName{"../textFiles/day2final.txt"};
+    partOne(fileName);
+}
+
+void partOne(std::string fileName)
+{
+    std::ifstream file{fileName};
+
+    if (file.is_open())
+    {
+        std::string line{};
+        std::vector<int> codes{};
+        std::string tok{};
+        while (std::getline(file, line))
+        {
+            std::stringstream ss{line};
+
+            while (std::getline(ss, tok, ','))
+            {
+                codes.push_back(std::stoi(tok));
+            }
+        }
+        int currentPos{0};
+        codes[1] = 12;
+        while (codes[currentPos] != 99)
+        {
+            int a{codes[codes[currentPos + 1]]};
+            int b{codes[codes[currentPos + 2]]};
+            if (codes[currentPos] == 1)
+            {
+                codes[codes[currentPos + 3]] = a + b;
+            }
+            else if (codes[currentPos] == 2)
+            {
+                codes[codes[currentPos + 3]] = a * b;
+            }
+            currentPos += 4;
+        }
+        std::cout << "day 2 part one: " << codes[0] << std::endl;
+    }
+    else
+    {
+        std::cout << "file won't open" << std::endl;
+    }
+}
---
+++ 2019/CPP Feb 2025/Day 2/day2.h
@@ -0,0 +1,8 @@
+#ifndef day2_h
+#define day2_h
+
+#include <string>
+
+void partOne(std::string fileName);
+
+#endif
