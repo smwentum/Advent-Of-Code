1e6c8719e34a321a9a222087afb7cea483d28e38
Sai Wentum <michael.wentum@gmail.com>
2025-11-12 07:29:07 -0500

    parsed the input now just need to solve part 1

--- 2023/cpp Nov 2025/AdventOfCode/Day6.cpp
+++ 2023/cpp Nov 2025/AdventOfCode/Day6.cpp
@@ -6,8 +6,47 @@ void Day6PartOne()
 {
 	//get the data 
 	vector<string> lines = getThelines(); 
+	
+	vector<int> times{ getNumbersFromLinePt6(lines[0]) };
+	vector<int> distances{ getNumbersFromLinePt6(lines[1]) };
+
+	for (auto time : times)
+	{
+		cout << time << endl; 
+	}
+
+	for (auto distance : distances)
+	{
+		cout << distance << endl; 
+	}
+	
+
+	
 
 }
+
+vector<int> getNumbersFromLinePt6(string line)
+{
+	vector<int> numbers{};
+	line = line.substr(line.find_first_of(':')+1);
+	line = line.substr(line.find_first_not_of(' '));
+	cout << line << endl; 
+	stringstream ss(line);
+	string numberString;
+	
+
+	while (getline(ss, numberString, ' '))
+	{
+		//cout << numberString << endl;
+		if (isdigit(numberString[0]))
+		{
+			numbers.push_back(stoi(numberString));
+		}
+	}
+	return numbers;
+}
+
+
 
 
 vector<string> getThelines()
@@ -19,7 +58,8 @@ vector<string> getThelines()
 	string line{};
 	while (getline(file, line))
 	{
-		cout << line << endl;
+		//cout << line << endl;
+		lines.push_back(line);
 	}
 
 
--- 2023/cpp Nov 2025/AdventOfCode/Day6.h
+++ 2023/cpp Nov 2025/AdventOfCode/Day6.h
@@ -1,11 +1,15 @@
 #pragma once
 
+#include <algorithm>
 #include <fstream>
-
 #include <iostream>
+#include <queue>
+#include <sstream>
 #include <string>
 #include <vector>
 
 
+
 void Day6PartOne();
 std::vector<std::string> getThelines();
+std::vector<int> getNumbersFromLinePt6(std::string line);
