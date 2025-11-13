0bb2231e72d5b7ade826d0eda23d4dcab0b2e82b
Sai Wentum <michael.wentum@gmail.com>
2025-11-13 07:17:51 -0500

    added constructor

--- 2023/cpp Nov 2025/AdventOfCode/CamelHand.cpp
+++ 2023/cpp Nov 2025/AdventOfCode/CamelHand.cpp
@@ -1,1 +1,10 @@
 #include "CamelHand.h"
+
+using namespace std; 
+
+CamelHand::CamelHand(string hand, long long bid)
+{
+	this->hand = hand; 
+	this->bid = bid; 
+
+}
--- 2023/cpp Nov 2025/AdventOfCode/CamelHand.h
+++ 2023/cpp Nov 2025/AdventOfCode/CamelHand.h
@@ -1,5 +1,15 @@
 #pragma once
+
+#include <string>
+#include <vector>
+#include <iostream>
+
 class CamelHand
 {
+	public:
+		CamelHand(std::string hand, long long bid);
+	private:
+		string hand; 
+		long long bid; 
 };
 
