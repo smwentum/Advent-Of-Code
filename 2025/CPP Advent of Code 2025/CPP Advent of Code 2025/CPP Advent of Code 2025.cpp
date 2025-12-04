daaadf30d1d0f6a4d958e9f696b04b5fafa3799b
Sai Wentum <michael.wentum@gmail.com>
2025-12-04 06:50:00 -0500

    did day 3 part 1

--- 2025/CPP Advent of Code 2025/CPP Advent of Code 2025/CPP Advent of Code 2025.cpp
+++ 2025/CPP Advent of Code 2025/CPP Advent of Code 2025/CPP Advent of Code 2025.cpp
@@ -5,6 +5,7 @@
 
 #include "Day1.h"
 #include "Day2.h"
+#include "Day3.h"
 #include "Day4.h"
 
 using namespace std; 
@@ -14,6 +15,12 @@ int main()
 	cout << "Day 1" << endl; 
 	Day1PartOne();
 	Day1PartTwo();
+	cout << "Day 2" << endl; 
+	//DayTwoPartOne(); 
+	cout << "i have day 2, but it slow, just for now i am going to leave it commented out" << endl; 
+	//DayTwoPartTwo();
+	cout << "Day 3" << endl;
+	Day3PartOne(); 
 	cout << "Day 2" << endl; 
 	//DayTwoPartOne(); 
 	//DayTwoPartTwo();
