56f55e109518cb3be2b7984566b0b9ad75940f23
Sai Wentum <michael.wentum@gmail.com>
2025-12-06 20:28:38 -0500

    done

--- 2025/CPP Advent of Code 2025/CPP Advent of Code 2025/Day3.cpp
+++ 2025/CPP Advent of Code 2025/CPP Advent of Code 2025/Day3.cpp
@@ -2,6 +2,9 @@
 
 using namespace std; 
 
+
+static std::map<std::tuple<std::string, int>, std::string> dict;
+
 void Day3PartOne()
 {
 	vector<string> batteryBanks = getDay3PartOneInput();
@@ -28,30 +31,63 @@ int getBestTwoBatteriesFromBank(string bb)
 	return bestAnswer;
 }
 
-//i am going to guess a leetcode code trick of i think montonic stack (i think i am using the wrong term)
+
 long long getBestTwelveBatteriesFromBank(string bb)
 {
-	//start from the end 
-	char bigestSecondDigit = bb[bb.length() - 1];
-	long long bestAnswer = 0;
-	for (int i = bb.length() - 2; i >= 0; i--)
-	{
-		string myGuess = { bb[i],bigestSecondDigit };
-		bestAnswer = max(bestAnswer, stoll(myGuess));
-		bigestSecondDigit = max(bb[i], bigestSecondDigit);
-	}
-	return bestAnswer;
+	//after doing some research i am going to try to do the problem with recursion
+	return stoll(getBestNBattriesFromBank(bb, 12));
+	
+	
 }
+
+string getBestNBattriesFromBank(string bank, int num)
+{
+	auto t = tuple<string, int>(bank, num);
+	if (num == 0)
+	{
+		return "";
+	}
+	if (dict.contains(t))
+	{
+		return dict[t];
+	}
+	if (bank.length() == num)
+	{
+		//there aren't any choices so i just go with that
+		dict[t] = bank; 
+	}
+	else if (bank.length() > num)
+	{
+		string s = "";
+		s = myMax(bank[0] + getBestNBattriesFromBank(bank.substr(1), num - 1), getBestNBattriesFromBank(bank.substr(1), num));
+		dict[t] = s; 
+	}
+	else
+	{
+		cout << "something is wrong" << endl; 
+	}
+	return dict[t];
+}
+
+string myMax(string s1, string s2)
+{
+	if (stoll(s1) >= stoll(s2))
+	{
+		return s1; 
+	}
+	return s2; 
+}
+
 
 void Day3PartTwo()
 {
 	vector<string> batteryBanks = getDay3PartOneInput();
-	int total = 0;
+	long long total = 0;
 	for (auto batterybank : batteryBanks)
 	{
-		total += getBestTwoBatteriesFromBank(batterybank);
+		total += getBestTwelveBatteriesFromBank(batterybank);
 	}
-	cout << "Day 3 part 1: " << total << endl;
+	cout << "Day 3 part 2: " << total << endl;
 }
 
 
@@ -59,7 +95,7 @@ vector<string> getDay3PartOneInput()
 {
 	vector<string> lines; 
 
-	ifstream file("Day3a.txt");
+	ifstream file("Day3.txt");
 	string line; 
 	while (getline(file, line))
 	{
--- 2025/CPP Advent of Code 2025/CPP Advent of Code 2025/Day3.h
+++ 2025/CPP Advent of Code 2025/CPP Advent of Code 2025/Day3.h
@@ -5,8 +5,15 @@
 #include<iostream>
 #include<fstream>
 #include<algorithm>
+#include<map>
+#include<tuple>
+
+
 
 void Day3PartOne(); 
 void Day3PartTwo();
 std::vector<std::string> getDay3PartOneInput();
 int getBestTwoBatteriesFromBank(std::string bb);
+std::string myMax(std::string s1, std::string s2);
+std::string getBestNBattriesFromBank(std::string bank, int num);
+long long getBestTwelveBatteriesFromBank(std::string bb);
